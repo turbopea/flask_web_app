07840401303c51f73be5ca68b0138ce65dbbcdc6
Eriks <erikszirnis11@gmail.com>
2026-05-03 21:16:00 +0300

    Added README.md, added server.py which contains Flask logic. Created folders static and templates and added index.html to templates. Added styles.css to static folder.

---
+++ README.md
@@ -0,0 +1,28 @@
+# FLASK-WEB-APP
+
+This is my simple Flask web app. Stores input data in MongoDB. Practicing to create a docker image/containers with this app and pushing them to Amazon ECR. 
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+To be added
+```
+
+## Usage
+
+```python
+To be added
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+What kind of license is necessary for this?
---
+++ server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import pymongo
+from flask import Flask, request, render_template
+
+app=Flask(__name__)
+myclient = pymongo.MongoClient("mongodb://admin:pass@mongo:27017/")
+database = myclient["CV-database"]
+columns = database["CVdictionary"]
+
+@app.route("/", methods=["GET","POST"])
+
+def home():
+    if request.method == "POST"
+        name = request.form.get("name")
+        email = request.form.get("email")
+        interests = request.form.get("interests")
+
+        mydict = {"name": name, "email": email, "interests": interests}
+        columns.insert_one(mydict)
+
+    return render_template("index.html")
+
+app.run(host="0.0.0.0", port=5000)
